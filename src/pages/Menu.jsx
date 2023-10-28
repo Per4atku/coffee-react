083404a2b2b253fd49da97e67a2b17e377c33b1c
Per4atku <artcchi@gmail.com>
2023-10-28 11:50:04 +1000

    🔧 Bugfix. {category.name}

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -11,7 +11,7 @@ const Menu = () => {
         <div className="flex flex-col w-full max-w-[1300px] mx-auto">
           {products.map((category, index) => (
             <div key={index}>
-              <h4 className="font-bold text-xl text-center py-2 mt-8">Whole beans</h4>
+              <h4 className="font-bold text-xl text-center py-2 mt-8">{category.name}</h4>
               <div className="grid grid-cols-1 gap-y-4 border-t border-solid border-p-gray pt-4 xs:gap-y-8 sm:grid-cols-2 md:grid-cols-[256px,256px] md:gap-x-8 md:mx-auto lg:grid-cols-2 lg:mx-0 min-[1100px]:grid-cols-3 xl:w-full">
                 {category.list.map((product, index) => (
                   <ProductCard
