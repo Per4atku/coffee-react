4b277b63cdfc4ec0e83bb511e73cf79601a53345
Per4atku <artcchi@gmail.com>
2023-10-28 15:25:43 +1000

    CarContent done (!not responsive)

--- src/partials/Navbar.jsx
+++ src/partials/Navbar.jsx
@@ -1,45 +1,36 @@
 import { useState } from "react";
-import { HiOutlineX, HiOutlineMenu } from "react-icons/hi";
+import { HiOutlineX, HiOutlineMenu, HiOutlineArrowNarrowRight } from "react-icons/hi";
 import { Link } from "react-router-dom";
 
 const Cart = ({ itemsCount }) => {
+  const [toggleCart, setToggleCart] = useState(false);
   return (
-    <div
-      className="flex fixed bottom-2 right-5 w-12 h-12 rounded-full shadow-3xl bg-white items-center justify-center
-                    md:static md:bg-transparent md:shadow-none">
-      <div className="cursor-pointer relative right-0.5 bottom-0.5">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="28"
-          height="26"
-          viewBox="0 0 28 26"
-          fill="none">
-          <path
-            d="M5.66669 6.33334H24.0534C24.4259 6.33336 24.7943 6.41144 25.1349 6.56255C25.4754 6.71366 25.7805 6.93446 26.0305 7.2107C26.2805 7.48694 26.4698 7.81249 26.5863 8.16638C26.7027 8.52027 26.7438 8.89463 26.7067 9.26534L25.9067 17.2653C25.8409 17.9234 25.5329 18.5334 25.0426 18.9772C24.5523 19.4209 23.9146 19.6667 23.2534 19.6667H10.52C9.90332 19.6669 9.30559 19.4534 8.82861 19.0625C8.35164 18.6716 8.02489 18.1274 7.90402 17.5227L5.66669 6.33334Z"
-            stroke="#12463A"
-            stroke-width="2"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M20.3334 25H23M5.66669 6.33333L4.58669 2.00933C4.51446 1.721 4.34796 1.46508 4.11363 1.28221C3.8793 1.09934 3.59059 1.00001 3.29335 1H1.66669L5.66669 6.33333ZM9.66669 25H12.3334H9.66669Z"
-            stroke="#12463A"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-        {itemsCount > 0 && (
-          <svg
-            className="absolute -right-1 top-0"
-            xmlns="http://www.w3.org/2000/svg"
-            width="10"
-            height="10"
-            viewBox="0 0 10 10"
-            fill="none">
-            <circle cx="5" cy="5" r="4.5" fill="#E2EDE9" stroke="#4BB79E" />
-          </svg>
-        )}
+    <div className=" fixed bottom-2 right-5 md:relative">
+      <div
+        className="flex w-12 h-12 rounded-full shadow-3xl bg-white items-center justify-center
+                   md:bg-transparent md:shadow-none">
+        <button className="relative right-0.5" onClick={() => setToggleCart(!toggleCart)}>
+          {toggleCart ? (
+            <HiOutlineX size={30} color={"#12463a"} className="relative left-0.5" />
+          ) : (
+            <div>
+              <img src="/assets/ui/cart-button.svg" alt="Cart button" />
+              {itemsCount > 0 && (
+                <svg
+                  className="absolute -right-1 top-0"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  height="10"
+                  viewBox="0 0 10 10"
+                  fill="none">
+                  <circle cx="5" cy="5" r="4.5" fill="#E2EDE9" stroke="#4BB79E" />
+                </svg>
+              )}
+            </div>
+          )}
+        </button>
       </div>
+      {toggleCart && <CartContent setToggleCart={setToggleCart} />}
     </div>
   );
 };
@@ -103,6 +94,45 @@ const Navbar = () => {
         </div>
       )}
     </nav>
+  );
+};
+
+const CartContent = ({ setToggleCart }) => {
+  return (
+    <div className="bg-white w-[280px] rounded-2xl absolute bottom-0 right-0 md:right-0 md:top-12 md:bottom-auto">
+      <div>
+        <h4 className="font-bold p-4">Cart items (3)</h4>
+        <ul className="px-2">
+          <li className="flex items-center justify-between">
+            <img
+              src="https://i.ibb.co/FVRVLSV/latte.png"
+              alt="Vanilla Latte image"
+              className=" w-20"
+            />
+            <span>
+              Vanilla latte <b className="text-accent font-bold">(Hot)</b>
+            </span>
+            <div className="bg-black h-[1px] w-4"></div>
+            <div className="font-medium text-accent">23$</div>
+            <button>
+              <HiOutlineX size={20} />
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div className="p-4 ">
+        <div className="flex justify-between border-t border-black border-solid py-3">
+          <h4 className="text-xl font-medium text-black">Total:</h4>
+          <div className="text-xl font-bold text-accent">23$</div>
+        </div>
+        <a href="#" className="  text-accent font-medium text-lg underline">
+          Proceed to checkout ➝
+        </a>
+      </div>
+      <button className="absolute right-4 bottom-4 md:hidden">
+        <HiOutlineX size={30} onClick={() => setToggleCart(false)} />
+      </button>
+    </div>
   );
 };
 
